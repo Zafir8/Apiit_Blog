2d9df5ae484775838313947fcfe0893c484fd4f8
Mohamed Zafir Sharaz <zafirsharaz@gmail.com>
2024-03-10 01:10:48 +0530

    header & footer

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,17 @@
+<footer class="text-sm flex items-center border-t border-gray-100 justify-between py-4">
+    <!-- Larger Image and Copyright -->
+    <div class="flex items-center">
+        <img src="https://apiit.lk/wp-content/uploads/2023/05/apiit-logo_Courses.png" alt="Copyright" class="h-10 w-auto mx-2">
+        <span class="text-gray-500">&copy; 2024 Apiit Lanka. All rights reserved.</span>
+    </div>
+
+    <!-- Smaller Links -->
+    <div class="space-x-4">
+        <a class="text-gray-500 hover:text-blue-950 text-sm" href="">About Us</a>
+        <a class="text-gray-500 hover:text-blue-950 text-sm" href="">Help</a>
+        <a class="text-gray-500 hover:text-blue-950 text-sm" href="">Login</a>
+        <a class="text-gray-500 hover:text-blue-950 text-sm" href="">Explore</a>
+    </div>
+</footer>
+
+
---
+++ resources/views/layouts/partials/header.blade.php
@@ -0,0 +1,56 @@
+<header class="flex items-center justify-between py-3 px-6 border-b border-gray-100">
+    <div id="header-left" class="flex items-center">
+        <div class="text-gray-800 font-semibold">
+            <!-- Logo from the internet -->
+            <img src="https://apiit.lk/wp-content/uploads/2023/05/apiit-logo_Courses.png" alt="Logo" class="h-10 w-auto">
+        </div>
+        <div class="top-menu ml-10">
+            <ul class="flex space-x-4">
+                <li>
+                    <a class="flex space-x-2 items-center hover:text-blue-950 text-sm text-gray-500"
+                       href="http://127.0.0.1:8000">
+                        Home
+                    </a>
+                </li>
+
+                <li>
+                    <a class="flex space-x-2 items-center hover:text-blue-950 text-sm text-gray-500"
+                       href="http://127.0.0.1:8000/blog">
+                        Blog
+                    </a>
+                </li>
+
+                <li>
+                    <a class="flex space-x-2 items-center hover:text-blue-950 text-sm text-gray-500"
+                       href="http://127.0.0.1:8000/blog">
+                        About Us
+                    </a>
+                </li>
+
+                <li>
+                    <a class="flex space-x-2 items-center hover:text-blue-950 text-sm text-gray-500"
+                       href="http://127.0.0.1:8000/blog">
+                        Contact Us
+                    </a>
+                </li>
+
+                <li>
+                    <a class="flex space-x-2 items-center hover:text-blue-950 text-sm text-gray-500"
+                       href="http://127.0.0.1:8000/blog">
+                        Terms
+                    </a>
+                </li>
+
+            </ul>
+        </div>
+    </div>
+    <div id="header-right" class="flex items-center md:space-x-6">
+        @guest
+            @include('layouts.partials.header-right-guest')
+        @endguest
+
+        @auth()
+            @include('layouts.partials.header-right-auth')
+        @endauth
+    </div>
+</header>
