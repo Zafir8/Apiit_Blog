7e342c50d5350f29c468ab3b9db5f52b21dc46d5
Mohamed Zafir Sharaz <zafirsharaz@gmail.com>
2024-03-16 18:03:18 +0530

    Home blade update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,12 @@
 <x-app-layout>
-    @section('hero')
-        <div class="w-full text-center py-32">
-            <h1 class="text-2xl md:text-3xl font-bold text-center lg:text-5xl text-gray-700">
-                Welcome to <span class="text-emerald-500">APIIT Blog! </span>
-            </h1>
-            <p class="text-gray-500 text-lg mt-1">Inspire love for learning!</p>
-            <a class="px-3 py-2 text-lg text-white bg-gray-800 rounded mt-5 inline-block"
-               href="http://127.0.0.1:8000/blog">Start
-                Reading</a>
+
+@section('hero')
+        <div class="h-screen flex items-center justify-center bg-cover bg-center" style="background-image: url('https://images.pexels.com/photos/3807755/pexels-photo-3807755.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2');">
+            <h1 class="text-white font-bold text-5xl">Our blogs</h1>
         </div>
+
     @endsection
-    
+
 
     <div class="mb-10 w-full">
         <div class="mb-16">
