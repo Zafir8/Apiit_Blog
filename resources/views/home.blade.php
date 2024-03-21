4f577590a3363d8ec1aadc8224f06c0e75a4f16e
Mohamed Zafir Sharaz <zafirsharaz@gmail.com>
2024-03-21 20:56:29 +0530

    homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -27,7 +27,7 @@
 
     <div class="mb-10 w-full">
         <div class="mb-16">
-            <h2 class="mt-16 mb-5 text-3xl text-blue-950 font-bold">Featured Posts</h2>
+            <h2 class="mt-16 mb-5 text-3xl text-blue-950 font-bold">Featured Blogs</h2>
             <div class="w-full">
                 <div class="grid grid-cols-3 gap-10 w-full">
                     @foreach ($featuredPosts as $post)
@@ -41,7 +41,7 @@
         </div>
         <hr>
 
-        <h2 class="mt-16 mb-5 text-3xl text-blue-950 font-bold">Latest Posts</h2>
+        <h2 class="mt-16 mb-5 text-3xl text-blue-950 font-bold">Latest Blogs</h2>
         <div class="w-full mb-5">
             <div class="grid grid-cols-3 gap-10 w-full">
                 @foreach ($latestPosts as $post)
@@ -52,4 +52,6 @@
         <a class="mt-10 block text-center text-lg text-blue-950 font-semibold" href="http://127.0.0.1:8000/blog">More
             Posts</a>
     </div>
+
+
 </x-app-layout>
